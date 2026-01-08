697bf6a6322e6e8236f39a632d3de4a3a426afc7
lawerhussein91-code <lawerhussein91@gmail.com>
2026-01-08 08:18:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,1 @@
-from flask import Flask, session
-import pandas as pd
 
-app = Flask(__name__)
-app.secret_key = "test"
-
-@app.route("/")
-def home():
-    return "APP IS RUNNING OK"
-
-@app.route("/test")
-def test():
-    df = pd.read_excel("Book1.xlsx")
-    return str(list(df.columns))
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
